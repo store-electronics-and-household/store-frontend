f7cc1b4dd9da40cd852656f3185ca4ebee346310
Sergey Sizov <vpnsizov@yandex.ru>
2023-11-07 17:07:46 +0300

    add ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,71 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended', // Добавляем рекомендуемые правила для TypeScript
+    'plugin:storybook/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:storybook/recommended'
+  ],
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      fragment: 'Fragment',
+      version: 'detect',
+      flowVersion: '0.53',
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:storybook/recommended"
+    propWrapperFunctions: [
+      'forbidExtraProps',
+      { property: 'freeze', object: 'Object' },
+      { property: 'myFavoriteWrapper' },
+      { property: 'forbidExtraProps', exact: true },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    componentWrapperFunctions: [
+      'observer',
+      { property: 'styled' },
+      { property: 'observer', object: 'Mobx' },
+      { property: 'observer', object: '<pragma>' },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    formComponents: [
+      'CustomForm',
+      { name: 'Form', formAttribute: 'endpoint' },
+    ],
+    linkComponents: [
+      'Hyperlink',
+      { name: 'Link', linkAttribute: 'to' },
+    ],
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+        jest: true,
+      },
+      files: ['.eslintrc.{js,cjs}', '**/*.spec.js', '**/*.spec.jsx'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  ],
+  parser: '@typescript-eslint/parser', // Добавляем парсер TypeScript
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json', // Путь к файлу tsconfig.json
+  },
+  plugins: ['react', 'jest', '@typescript-eslint'], // Добавляем плагин для TypeScript
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+};
