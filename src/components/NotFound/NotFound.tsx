80645379c40714f5e190eebd76f6cb171146d9b5
Sariolka <sariola@list.ru>
2023-11-19 01:04:48 +0300

    feat(pagenotfound): add

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './NotFound.css';
+import logo from '../../image/logo.svg';
 
 const NotFound: React.FC = () => {
-  return <section className='not-found'></section>;
+  return (
+    <section className='not-found'>
+      <img className='not-found__logo' src={logo} alt='логотип магазина' />
+      <h2 className='not-found__title'>Страница не найдена</h2>
+      <p className='not-found__info'>
+        В адресе есть ошибка или страница была удалена
+      </p>
+      <a className='not-found__btn' href='/main'>
+        Вернуться на главную
+      </a>
+    </section>
+  );
 };
 
 export default NotFound;
