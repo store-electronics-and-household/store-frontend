49a2299496b60af71355b7f341363a8320fdfef4
Sariolka <sariola@list.ru>
2023-11-20 03:41:26 +0300

    fix(faq,contacts,about): add header+footer

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import mapImg from '../../image/map.png';
 import { contactsData } from '../../utils/constants';
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
 
 const Contacts: React.FC = () => {
   return (
-    <section className='contacts'>
-      <Breadcrumb currentPlace='Контакты' />
-      <div className='contacts__container'>
-        <h1 className='contacts__header'>Контакты</h1>
-        <div className='contacts__wrap'>
-          <p className='contacts__info'>{contactsData.phoneNumber}</p>
-          <p className='contacts__phone-time'>
-            для заказов с доставкой по территории РФ принимаем звонки с 9:00 до
-            22:00 по Мск
-          </p>
+    <>
+      <Header />
+      <section className='contacts'>
+        <Breadcrumb currentPlace='Контакты' />
+        <div className='contacts__container'>
+          <h1 className='contacts__header'>Контакты</h1>
+          <div className='contacts__wrap'>
+            <p className='contacts__info'>{contactsData.phoneNumber}</p>
+            <p className='contacts__phone-time'>
+              для заказов с доставкой по территории РФ принимаем звонки с 9:00
+              до 22:00 по Мск
+            </p>
+          </div>
+          <div className='contacts__wrap'>
+            <p className='contacts__info'>{contactsData.email}</p>
+            <p className='contacts__phone-time'>
+              для заказов с доставкой по территории РФ принимаем звонки с 9:00
+              до 22:00 по Мск
+            </p>
+          </div>
+          <div className='contacts__map-block'>
+            <p className='contacts__adress'>{contactsData.adress}</p>
+            <img
+              className='contacts__map'
+              src={mapImg}
+              alt='карта проезда, адрес'
+            />
+          </div>
         </div>
-        <div className='contacts__wrap'>
-          <p className='contacts__info'>{contactsData.email}</p>
-          <p className='contacts__phone-time'>
-            для заказов с доставкой по территории РФ принимаем звонки с 9:00 до
-            22:00 по Мск
-          </p>
-        </div>
-        <div className='contacts__map-block'>
-          <p className='contacts__adress'>{contactsData.adress}</p>
-          <img
-            className='contacts__map'
-            src={mapImg}
-            alt='карта проезда, адрес'
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+      <Footer />
+    </>
   );
 };
 
--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import Accordion from '../Accordion/Accordion';
 import { faqList } from '../../utils/constants';
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
 
 const Faq: React.FC = () => {
   return (
-    <section className='faq'>
-      <Breadcrumb currentPlace='Часто задаваемые вопросы' />
-      <div className='faq__container'>
-        <h1 className='faq__header'>Частые вопросы</h1>
-        <Accordion faqList={faqList.deliveryFaq} headText='Доставка:' />
-        <Accordion
-          faqList={faqList.characteristicFaq}
-          headText='Наличие и характеристики товара:'
-        />
-        <Accordion
-          faqList={faqList.returnsFaq}
-          headText='Обмен и возврат товара:'
-        />
-      </div>
-    </section>
+    <>
+      <Header />
+      <section className='faq'>
+        <Breadcrumb currentPlace='Часто задаваемые вопросы' />
+        <div className='faq__container'>
+          <h1 className='faq__header'>Частые вопросы</h1>
+          <Accordion faqList={faqList.deliveryFaq} headText='Доставка:' />
+          <Accordion
+            faqList={faqList.characteristicFaq}
+            headText='Наличие и характеристики товара:'
+          />
+          <Accordion
+            faqList={faqList.returnsFaq}
+            headText='Обмен и возврат товара:'
+          />
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
 
