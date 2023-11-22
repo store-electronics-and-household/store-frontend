f23905919a60a264e64a24929f79d630b9d82847
Anna Polyukhovich <blue.camomile.art@gmail.com>
2023-11-22 19:27:39 +0300

    feat: favourites layout

---
+++ src/utils/mocks/favouriteMockedData.ts
@@ -0,0 +1,44 @@
+export const mockedFavouritesProducts = [
+  {
+    id: 1,
+    name: 'Холодильник Haier C2F637CXRG Silver',
+    originPrice: 80990,
+    salesPrice: 76990,
+    discount: 30,
+  },
+  {
+    id: 2,
+    name: 'Портативная колонка Marshall Stockwell Black',
+    originPrice: 38990,
+    salesPrice: 0,
+    discount: 30,
+  },
+  {
+    id: 3,
+    name: 'Умная колонка Яндекс Станция 2 с Алисой, кобальт (YNDX-00051B)',
+    originPrice: 21990,
+    salesPrice: 0,
+    discount: 30,
+  },
+  {
+    id: 4,
+    name: 'Портативная колонка harman/kardon Go + Play Mini White',
+    originPrice: 45990,
+    salesPrice: 33990,
+    discount: 30,
+  },
+  {
+    id: 5,
+    name: 'Телевизор Samsung Series 5 UE32M5550AU, 32"(81 см), FHD',
+    originPrice: 60990,
+    salesPrice: 53660,
+    discount: 30,
+  },
+  {
+    id: 6,
+    name: 'Телевизор Samsung Series 5 UE32M5550AU, 32"(81 см), FHD',
+    originPrice: 60990,
+    salesPrice: 53660,
+    discount: 30,
+  }
+];
