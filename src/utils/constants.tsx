48b031f82b09ff8e39d68d03d8fdbdd3b8633bf8
Sariolka <sariola@list.ru>
2023-11-23 13:34:54 +0300

    fix(constants): fix

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -10,7 +10,8 @@ export const categoriesList = [
   {
     catId: 2,
     catTitle: 'Телефоны и аксессуары',
-    catImg: '../images/Vector.png',
+    catImg:
+      'https://images.unsplash.com/photo-1585060544812-6b45742d762f?q=80&w=1762&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     catUrl: '/categories',
   },
   {
