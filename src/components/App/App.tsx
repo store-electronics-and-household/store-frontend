9f9c14e66bfbc54ddb19a832f8ead5dbedcb91b8
Andrey Zhukov <settler59@yandex.ru>
2023-11-23 08:43:40 +0100

    fix: resolve conlicts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import './App.css';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Header from '../Header/Header';
