fd0e1d2c96e825ab4105c1648ec4858a69bdf642
Sariolka <sariola@list.ru>
2023-11-29 17:11:47 +0300

    style: change

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -6,100 +6,100 @@ import { PaginatedItems } from '../Pagination/Pagination';
 import { product } from '../../utils/constants';
 
 const Catalog: React.FC<{
-	id?: number;
-	name?: string;
-	originPrice?: number;
-	salesPrice?: number;
-	discount?: number;
-	url?: string;
+  id?: number;
+  name?: string;
+  originPrice?: number;
+  salesPrice?: number;
+  discount?: number;
+  url?: string;
 }> = () => {
-	return (
-		<>
-			<section className='catalog'>
-				<Breadcrumb currentPlace='Apple IPhone'>
-					<BreadcrumbItem
-						breadcrumbText='Смартфоны'
-						breadcrumbPath='/categories'
-					/>
-				</Breadcrumb>
-				<div className='catalog__container-big'>
-					<h1 className='catalog__title'>Apple IPhone</h1>
-					<div className='catalog__container'>
-						<ul className='catalog__collection'>
-							<li className='catalog__models'>
-								<a className='catalog__model-link' href=''>
-									Модель
-								</a>
-								<ul className='catalog__models'>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 15
-										</a>
-									</li>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 15 Pro
-										</a>
-									</li>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 15 Pro Max
-										</a>
-									</li>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 15 Plus
-										</a>
-									</li>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 14
-										</a>
-									</li>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 14 Pro
-										</a>
-									</li>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 14 Pro Max
-										</a>
-									</li>
-									<li className='catalog__model'>
-										<a className='catalog__model-link' href=''>
-											IPhone 14 Plus
-										</a>
-									</li>
-								</ul>
-							</li>
-							<ul className='catalog__models'>
-								<a className='catalog__model-link' href=''>
-									Другие модели
-								</a>
-							</ul>
-						</ul>
-						<ul className='catalog__rendered-list'>
-							<PaginatedItems
-								itemsPerPage={12}
-								items={product.map((product) => (
-									<ProductCardMedium
-										key={product.id}
-										name={product.name}
-										originPrice={product.originPrice}
-										salesPrice={product.salesPrice}
-										discount={product.discount}
-										url={''}
-									/>
-								))}
-							/>
-						</ul>
-					</div>
-				</div>
-			</section>
-			;
-		</>
-	);
+  return (
+    <>
+      <section className='catalog'>
+        <Breadcrumb currentPlace='Apple IPhone'>
+          <BreadcrumbItem
+            breadcrumbText='Смартфоны'
+            breadcrumbPath='/categories'
+          />
+        </Breadcrumb>
+        <div className='catalog__container-big'>
+          <h1 className='catalog__title'>Apple IPhone</h1>
+          <div className='catalog__container'>
+            <ul className='catalog__collection'>
+              <li className='catalog__models'>
+                <a className='catalog__model-link' href=''>
+                  Модель
+                </a>
+                <ul className='catalog__models'>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 15
+                    </a>
+                  </li>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 15 Pro
+                    </a>
+                  </li>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 15 Pro Max
+                    </a>
+                  </li>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 15 Plus
+                    </a>
+                  </li>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 14
+                    </a>
+                  </li>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 14 Pro
+                    </a>
+                  </li>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 14 Pro Max
+                    </a>
+                  </li>
+                  <li className='catalog__model'>
+                    <a className='catalog__model-link' href=''>
+                      IPhone 14 Plus
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <ul className='catalog__models'>
+                <a className='catalog__model-link' href=''>
+                  Другие модели
+                </a>
+              </ul>
+            </ul>
+            <ul className='catalog__rendered-list'>
+              <PaginatedItems
+                itemsPerPage={12}
+                items={product.map((product) => (
+                  <ProductCardMedium
+                    key={product.id}
+                    name={product.name}
+                    originPrice={product.originPrice}
+                    salesPrice={product.salesPrice}
+                    discount={product.discount}
+                    url={''}
+                  />
+                ))}
+              />
+            </ul>
+          </div>
+        </div>
+      </section>
+      ;
+    </>
+  );
 };
 
 export default Catalog;
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -3,8 +3,7 @@ export const categoriesList = [
   {
     catId: 1,
     catTitle: 'Игровые приставки',
-    catImg:
-      'https://images.unsplash.com/photo-1621259182978-fbf93132d53d?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    catImg: '$imagesDir/categories-mainPage/1.png',
     catUrl: '/categories',
   },
   {
