18341821d42a967163264744cfff843bf318f676
Sergey Sizov <vpnsizov@yandex.ru>
2023-12-08 12:04:02 +0300

    new changes

--- src/components/PasswordRecovery/PasswordRecovery.tsx
+++ src/components/PasswordRecovery/PasswordRecovery.tsx
@@ -14,7 +14,9 @@ const PasswordRecovery: React.FC<PasswordRecoveryProps> = ({
   onOpenRecoveryPopup,
   isOpenPasswordRecovery,
 }) => {
-  const [isNext, setisNext] = React.useState<boolean>(false);
+  const [isNext, SetIsNext] = React.useState<boolean>(false);
+  const [isEmailValidationEnabled, setEmailValidationEnabled] =
+    React.useState<boolean>(false);
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
@@ -50,8 +52,13 @@ const PasswordRecovery: React.FC<PasswordRecoveryProps> = ({
     formik.resetForm();
   };
 
-  const handleNextLevel = (): void => {
-    setisNext(true);
+  const handleClickNextBtn = (): void => {
+    setEmailValidationEnabled(true);
+    if (formik.errors.loginRecovery) {
+      SetIsNext(true);
+    } else {
+      console.log('');
+    }
   };
 
   return (
@@ -73,105 +80,115 @@ const PasswordRecovery: React.FC<PasswordRecoveryProps> = ({
         >
           <div className='pass-recovery__title-container'>
             <div className='pass-recovery__title'>Восстановление пароля</div>
-            {!isNext ? (
+            {!isNext && (
               <p className='pass-recovery__subtitle'>
                 Введите почту, чтобы мы смогли найти ваш аккаунт
               </p>
-            ) : (
-              ''
             )}
           </div>
           <div className='pass-recovery__inputs'>
-            <div className='pass-recovery__input-wrapper'>
-              <label className='pass-recovery__label'>Почта</label>
-              <input
-                className={`pass-recovery__input ${
-                  formik.submitCount > 0 &&
-                  formik.touched.loginRecovery &&
-                  formik.errors.loginRecovery
-                    ? 'pass-recovery__input_invalid'
-                    : formik.touched.loginRecovery && formik.submitCount > 0
+            {!isNext && (
+              <div className='pass-recovery__input-wrapper'>
+                <label className='pass-recovery__label'>Почта</label>
+                <input
+                  className={`pass-recovery__input ${
+                    isEmailValidationEnabled &&
+                    formik.touched.loginRecovery &&
+                    formik.errors.loginRecovery
+                      ? 'pass-recovery__input_invalid'
+                      : formik.touched.loginRecovery
                       ? 'pass-recovery__input_valid'
                       : ''
-                }`}
-                type='text'
-                name='loginRecovery'
-                value={formik.values.loginRecovery}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                minLength={6}
-                required
-              />
+                  }`}
+                  type='text'
+                  name='loginRecovery'
+                  value={formik.values.loginRecovery}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  minLength={6}
+                  required
+                />
+                {isEmailValidationEnabled &&
+                  formik.touched.loginRecovery &&
+                  formik.errors.loginRecovery && (
+                    <span className='pass-recovery__error-text'>
+                      {formik.errors.loginRecovery}
+                    </span>
+                  )}
+              </div>
+            )}
+            {isNext && (
+              <>
+                <div className='pass-recovery__input-wrapper'>
+                  <input
+                    className={`pass-recovery__input ${
+                      formik.touched.passwordRecovery &&
+                      formik.errors.passwordRecovery
+                        ? 'pass-recovery__input_invalid'
+                        : formik.touched.passwordRecovery
+                        ? 'pass-recovery__input_valid'
+                        : ''
+                    }`}
+                    placeholder='Пароль'
+                    type='password'
+                    name='passwordRecovery'
+                    value={formik.values.passwordRecovery}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    required
+                  />
+                  {formik.touched.passwordRecovery &&
+                    formik.errors.passwordRecovery && (
+                      <span className='pass-recovery__error-text'>
+                        {formik.errors.passwordRecovery}
+                      </span>
+                    )}
+                </div>
+                <div className='pass-recovery__input-wrapper'>
+                  <input
+                    className={`pass-recovery__input ${
+                      formik.touched.confirmPasswordRecovery &&
+                      formik.errors.confirmPasswordRecovery
+                        ? 'pass-recovery__input_invalid'
+                        : formik.touched.confirmPasswordRecovery
+                        ? 'pass-recovery__input_valid'
+                        : ''
+                    }`}
+                    placeholder='Повторите пароль'
+                    type='password'
+                    name='confirmPasswordRecovery'
+                    value={formik.values.confirmPasswordRecovery}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    required
+                  />
+                  {formik.touched.confirmPasswordRecovery &&
+                    formik.errors.confirmPasswordRecovery && (
+                      <span className='pass-recovery__error-text'>
+                        {formik.errors.confirmPasswordRecovery}
+                      </span>
+                    )}
+                </div>
+              </>
+            )}
+            <div className='pass-recovery__buttons'>
+              {isNext && (
+                <button
+                  className='pass-recovery__button signin__button_enter'
+                  type='submit'
+                >
+                  Восстановить
+                </button>
+              )}
+              {!isNext && (
+                <button
+                  className='pass-recovery__button signin__button_enter'
+                  onClick={handleClickNextBtn}
+                >
+                  Далее
+                </button>
+              )}
             </div>
-            {formik.submitCount > 0 &&
-              formik.touched.loginRecovery &&
-              formik.errors.loginRecovery && (
-                <span className='pass-recovery__error-text'>
-                  {formik.errors.loginRecovery}
-                </span>
-              )}
-
-            <input
-              className={`pass-recovery__input ${
-                formik.touched.passwordRecovery &&
-                formik.errors.passwordRecovery
-                  ? 'pass-recovery__input_invalid'
-                  : formik.touched.passwordRecovery
-                    ? 'pass-recovery__input_valid'
-                    : ''
-              }`}
-              placeholder='Пароль'
-              type='password'
-              name='passwordRecovery'
-              value={formik.values.passwordRecovery}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              required
-            />
-            {formik.touched.passwordRecovery &&
-              formik.errors.passwordRecovery && (
-                <span className='pass-recovery__error-text'>
-                  {formik.errors.passwordRecovery}
-                </span>
-              )}
-            <input
-              className={`pass-recovery__input ${
-                formik.touched.confirmPasswordRecovery &&
-                formik.errors.confirmPasswordRecovery
-                  ? 'pass-recovery__input_invalid'
-                  : formik.touched.confirmPasswordRecovery
-                    ? 'pass-recovery__input_valid'
-                    : ''
-              }`}
-              placeholder='Повторите пароль'
-              type='password'
-              name='confirmPasswordRecovery'
-              value={formik.values.confirmPasswordRecovery}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              required
-            />
-            {formik.touched.confirmPasswordRecovery &&
-              formik.errors.confirmPasswordRecovery && (
-                <span className='pass-recovery__error-text'>
-                  {formik.errors.confirmPasswordRecovery}
-                </span>
-              )}
-          </div>
-          <div className='pass-recovery__buttons'>
-            <button
-              className='pass-recovery__button signin__button_enter'
-              disabled={!(formik.dirty && formik.isValid)}
-              type='submit'
-            >
-              Восстановить
-            </button>
-            <button
-              className='pass-recovery__button signin__button_enter'
-              onClick={handleNextLevel}
-            >
-              Далее
-            </button>
           </div>
         </form>
       </div>
