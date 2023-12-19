3b531dbd6d61daa787fae4fe808b981cdf484d64
Andrey Zhukov <settler59@yandex.ru>
2023-12-19 12:58:33 +0100

    feat: new base url

--- src/utils/api/user-api.ts
+++ src/utils/api/user-api.ts
@@ -64,16 +64,6 @@ export const getBanners = async (): Promise<any> => {
   });
 };
 
-// export const getSearchResults = async (id: number): Promise<any> => {
-//   return await request(`/collections/${id}`, {
-//     method: 'GET',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//   });
-// };
-
 export const changePassword = async (
   email: string,
   password: string
