34bf2852d39489d836fba3fbbef59817620e5e59
Kazzzemir <s1ckwell@mail.ru>
2023-12-25 15:28:06 +0300

    user_delete fix

--- src/utils/api/user-api.ts
+++ src/utils/api/user-api.ts
@@ -97,8 +97,8 @@ export const checkUserEmail = async (email: string): Promise<any> => {
       Accept: 'application/json',
     },
     body: JSON.stringify({
-      name: email
-    })
+      name: email,
+    }),
   });
 };
 
@@ -118,7 +118,7 @@ export const patchUser = async (
 };
 
 export const deleteUser = async (token: string): Promise<any> => {
-  return await request('/user', {
+  return await request('/auth/delete', {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
